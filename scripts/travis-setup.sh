b525fbc9ed06642b2e4bfc828c0b3cae45762b0f
Florian Walch <florian@fwalch.com>
2014-09-12 13:53:57 +0200

    TESTING

--- scripts/travis-setup.sh
+++ scripts/travis-setup.sh
@@ -1,6 +1,6 @@
 nightly-x64() {
   mkdir "$TRAVIS_BUILD_DIR/_neovim"
-  wget -q -O - https://github.com/neovim/neovim/releases/download/nightly/neovim-linux64.tar.gz \
+  wget -q -O - https://github.com/fwalch/neovim/releases/download/nightly/neovim-linux64.tar.gz \
     | tar xzf - --strip-components=1 -C "$TRAVIS_BUILD_DIR/_neovim"
 
   export PATH="$TRAVIS_BUILD_DIR/_neovim/bin:$PATH"
