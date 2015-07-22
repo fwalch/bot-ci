1e9db01a4b738d05b321e9f0b09fc78ba80d48f3
Florian Walch <florian@fwalch.com>
2015-07-22 11:58:01 +0300

    Remove code that is no longer required.
    Prebuilt dependencies are still used for OS X.

--- ci/common/deps-repo.sh
+++ ci/common/deps-repo.sh
@@ -2,16 +2,6 @@
 
 require_environment_variable BUILD_DIR "${BASH_SOURCE[0]}" ${LINENO}
 
-# Linux
-DEPS_LINUX64_REPO=${DEPS_LINUX64_REPO:-${DEPS_REPO:-neovim/deps}}
-DEPS_LINUX64_BRANCH=${DEPS_LINUX64_BRANCH:-${DEPS_BRANCH:-master}}
-DEPS_LINUX64_DIR=${DEPS_LINUX64_DIR:-${DEPS_DIR:-/opt/neovim-deps}}
-DEPS_LINUX64_SUBTREE=${DEPS_LINUX64_SUBTREE:-/linux-x64/}
-DEPS_LINUX32_REPO=${DEPS_LINUX32_REPO:-${DEPS_REPO:-neovim/deps}}
-DEPS_LINUX32_BRANCH=${DEPS_LINUX32_BRANCH:-${DEPS_BRANCH:-master}}
-DEPS_LINUX32_DIR=${DEPS_LINUX32_DIR:-${DEPS_DIR:-/opt/neovim-deps}}
-DEPS_LINUX32_SUBTREE=${DEPS_LINUX32_SUBTREE:-/linux-x86/}
-
 # OS X
 DEPS_OSX64_REPO=${DEPS_OSX64_REPO:-${DEPS_REPO:-neovim/deps}}
 DEPS_OSX64_BRANCH=${DEPS_OSX64_BRANCH:-${DEPS_BRANCH:-master}}
@@ -23,21 +13,6 @@ DEPS_SRC_REPO=${DEPS_SRC_REPO:-${DEPS_REPO:-neovim/deps}}
 DEPS_SRC_BRANCH=${DEPS_SRC_BRANCH:-${DEPS_BRANCH:-master}}
 DEPS_SRC_DIR=${DEPS_SRC_DIR:-${DEPS_DIR:-/opt/neovim-deps}}
 DEPS_SRC_SUBTREE=${DEPS_SRC_SUBTREE:-/src/}
-
-# Set up prebuilt 64-bit Neovim dependencies.
-setup_deps64() {
-  local deps_dir="DEPS_${CI_OS^^}64_DIR"
-  local deps_subtree="DEPS_${CI_OS^^}64_SUBTREE"
-  local deps_repo="DEPS_${CI_OS^^}64_REPO"
-  local deps_branch="DEPS_${CI_OS^^}64_BRANCH"
-
-  local deps_path="${!deps_dir}${!deps_subtree}"
-
-  NVIM_DEPS_REPO="${!deps_repo}" \
-  NVIM_DEPS_BRANCH="${!deps_branch}" \
-  source "${BUILD_DIR}/scripts/travis-setup.sh" && \
-  _setup_deps "${deps_path}"
-}
 
 # Build Neovim dependencies in an output directory.
 # ${1}: Variable prefix (DEPS_<OS>64/DEPS_<OS>32).
